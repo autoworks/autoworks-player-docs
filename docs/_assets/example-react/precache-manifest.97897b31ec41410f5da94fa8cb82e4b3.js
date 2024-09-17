0263f2272a632bf88e4595de47a5ac96b3ae4713
MantasMikal <mantas.codes@gmail.com>
2024-09-17 15:51:08 +0300

    feat: updates player version

---
+++ docs/_assets/example-react/precache-manifest.97897b31ec41410f5da94fa8cb82e4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6113ef28ddf36d85a02b3fc38462f69a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c91f24485bf314c7341d",
+    "url": "./static/css/2.d376823b.chunk.css"
+  },
+  {
+    "revision": "c91f24485bf314c7341d",
+    "url": "./static/js/2.4d495d0f.chunk.js"
+  },
+  {
+    "revision": "7f18ba06fedcb2b3dddab8f68d6ee71a",
+    "url": "./static/js/2.4d495d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035af911a5803bc7dc71",
+    "url": "./static/js/main.6bf7a7e5.chunk.js"
+  },
+  {
+    "revision": "72fc5273cf641eb83c1f",
+    "url": "./static/js/runtime-main.4b2f8507.js"
+  }
+]);
